e2d2cb4465a87576cf00ff7fc22258c01e4ba6ff
Jdevzy <jdevz520@gmail.com>
2025-11-21 14:58:41 +0800

    Work in progress before gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/my-portfolio/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
